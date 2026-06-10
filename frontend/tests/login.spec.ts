8840489c46ded1cab015d6f43089874d3e8f2919
Jędrzej Kubala <jedrzejkubala@gmail.com>
2026-06-10 15:32:30 +0200

    Revert "Insignificant change"
    This reverts commit c42030005b06242b0307446b390a48a5547cf4bd.

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -84,7 +84,6 @@ test("Successful log out", async ({ page }) => {
   await page.getByTestId("user-menu").click()
   await page.getByRole("menuitem", { name: "Log out" }).click()
   await page.waitForURL("/login")
-  // insignificant change
 })
 
 test("Logged-out user cannot access protected routes", async ({ page }) => {
